8772e4e750a93681933f9346cd72cce2536233bf
Jake Prins <jakeprins@outlook.com>
2023-10-28 09:23:20 +0200

    Update yarn.lock

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  moduleDirectories: ['node_modules', 'src'],
-  testEnvironment: 'jsdom'
-}
